b2b4b6cc3adfd96fcb228412d3c3bbb49037caf6
kuadalma <94044289+kuadalma@users.noreply.github.com>
2023-09-19 14:02:35 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,20 @@
-﻿using TicTacToe;
+using TicTacToe;
 Board board = new();
 SI SI = new();
 
 
-Console.WriteLine(board);
 while (true)
 {
     board = new();
+    //board.EnterMove("5", "O");
+    Console.WriteLine(board);
     while (true)
     {
         board.EnterMove(SI.FindBestMove(board.GetBoard()));
         Console.Clear();
         Console.WriteLine(board);
         if (board.VictoryCheck()) break;
-        board.EnterMove(int.Parse(Console.ReadLine()), "X");
+        board.EnterMove(Console.ReadLine(), "X");
         Console.Clear();
         Console.WriteLine(board);
         if (board.VictoryCheck()) break;
